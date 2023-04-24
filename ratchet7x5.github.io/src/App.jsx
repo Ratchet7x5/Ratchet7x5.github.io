9d142555460b2c04d781426c687f66ed18b746ec
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2023-04-24 18:50:34 +1200

    reformat files with prettier

--- ratchet7x5.github.io/src/App.jsx
+++ ratchet7x5.github.io/src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+// prettier-ignore
 function App() {
   const [count, setCount] = useState(0)
 
@@ -32,4 +32,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
