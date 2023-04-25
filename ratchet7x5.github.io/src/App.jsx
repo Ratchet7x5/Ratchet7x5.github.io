d515563261a37f8334f4278d0672ebe086661604
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2023-04-25 12:19:10 +1200

    add new card layout, add dummy content for testing

--- ratchet7x5.github.io/src/App.jsx
+++ ratchet7x5.github.io/src/App.jsx
@@ -2,34 +2,70 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-// prettier-ignore
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
-    <div className="App">
+    <div className="App space-y-4">
+      {/* div1 = About Me.  */}
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Hi, my name is Tarun Ramachandran. </p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="bg-neutral-950 rounded-lg">
+        <div className="mx-auto grid max-w-2xl items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+          <div className="gap-4 sm:gap-6">
+            <img
+              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg"
+              alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
+              className="bg-gray-100"
+            />
+          </div>
+          <div>
+            <h2 className="text-3xl font-bold tracking-tight text-gray-300 sm:text-4xl">
+              Technical Specifications
+            </h2>
+            <p className="mt-4 text-gray-300">
+              The walnut wood card tray is precision milled to perfectly fit a
+              stack of Focus cards. The powder coated steel divider separates
+              active cards from new ones, or can be used to archive important
+              task lists.
+            </p>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="bg-neutral-950">
+        <div className="mx-auto grid max-w-2xl items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+          <div className="gap-4 sm:gap-6">
+            <iframe
+              src="https://www.youtube.com/embed/tgbNymZ7vqY"
+              className="aspect-video w-full h-full"
+            />
+          </div>
+          <div>
+            <h2 className="text-3xl font-bold tracking-tight text-gray-300 sm:text-4xl">
+              Technical Specifications
+            </h2>
+            <p className="mt-4 text-gray-300">
+              The walnut wood card tray is precision milled to perfectly fit a
+              stack of Focus cards. <br/>The powder coated steel divider separates
+              active cards from new ones, or can be used to archive important
+              task lists.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
+
+/*
+todo: use these logos later
+<a href="https://vitejs.dev" target="_blank">
+  <img src={viteLogo} className="logo" alt="Vite logo" />
+</a>
+<a href="https://reactjs.org" target="_blank">
+  <img src={reactLogo} className="logo react" alt="React logo" />
+</a>
+*/
