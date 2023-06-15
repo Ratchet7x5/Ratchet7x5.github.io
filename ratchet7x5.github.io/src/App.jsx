d5739e95ed0fe49de4296351a6f1024468fbdce4
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2023-06-15 18:57:22 +1200

    add WIP indicator

--- ratchet7x5.github.io/src/App.jsx
+++ ratchet7x5.github.io/src/App.jsx
@@ -24,51 +24,21 @@ function App() {
       {/* soon-to-be image component */}
 
       <div className="rounded-lg bg-neutral-950 " id="projects">
-        <div className="mx-auto grid max-w-2xl items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
-          <div className="gap-4 sm:gap-6">
-            <img
-              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg"
-              alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
-              className="bg-gray-100"
-            />
+        <div className="mx-auto items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+          <div className="gap-1 sm:gap-6">
+            
           </div>
           <div>
             <h2 className="font-bold text-3xl tracking-tight text-gray-300 sm:text-4xl ">
-              Technical Specifications
+              Currently a work in progress
             </h2>
-            <p className="mt-4 text-gray-300">
-              The walnut wood card tray is precision milled to perfectly fit a
-              stack of Focus cards. The powder coated steel divider separates
-              active cards from new ones, or can be used to archive important
-              task lists.
-            </p>
           </div>
         </div>
       </div>
 
       {/* soon-to-be video component */}
 
-      <div className="rounded-lg bg-neutral-950">
-        <div className="mx-auto grid max-w-2xl items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
-          <div className="gap-4 sm:gap-6">
-            <iframe
-              src="https://www.youtube.com/embed/tgbNymZ7vqY"
-              className="aspect-video h-full w-full"
-            />
-          </div>
-          <div>
-            <h2 className="font-bold text-3xl tracking-tight text-gray-300 sm:text-4xl">
-              Technical Specifications
-            </h2>
-            <p className="mt-4 text-gray-300">
-              The walnut wood card tray is precision milled to perfectly fit a
-              stack of Focus cards. <br />
-              The powder coated steel divider separates active cards from new
-              ones, or can be used to archive important task lists.
-            </p>
-          </div>
-        </div>
-      </div>
+      
 
       {/* add footer here */}
       <footer className="rounded-lg bg-neutral-950">
