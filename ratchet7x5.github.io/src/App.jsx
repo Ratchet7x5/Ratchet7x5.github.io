c338b8f7d96648231bba83c0328853503f58ed62
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2023-08-22 18:30:22 +1200

    add new projects, add new images

--- ratchet7x5.github.io/src/App.jsx
+++ ratchet7x5.github.io/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import profpic from './assets/images/profile/480_No_BG.png';
 import dgapppic from './assets/images/projects/dgapp/NDA.png';
+import quirkymentors from './assets/images/projects/quirky-mentors/mentors.png';
 import './App.css';
 import ProjectImageCard from './components/common/projectImageCard';
 
@@ -19,9 +20,9 @@ function App() {
           </div>
           <div class="">
             <p class="mt-4 text-gray-300">
-              Hello, my name is Tarun Ramachandran. I'm a recent graduate from AUT
-              and I'm currently on the hunt for a backend software development
-              role.
+              Hello, my name is Tarun Ramachandran. I'm a recent graduate from
+              AUT and I'm currently on the hunt for a backend software
+              development role.
             </p>
           </div>
         </div>
@@ -34,7 +35,8 @@ function App() {
           <div className="gap-1 sm:gap-6"></div>
           <div>
             <h2 className="font-bold text-3xl tracking-tight text-gray-300 sm:text-4xl ">
-              Currently a work in progress. More projects will be added over time. 
+              Currently a work in progress. More projects will be added over
+              time.
             </h2>
           </div>
         </div>
@@ -43,14 +45,77 @@ function App() {
       {/* soon-to-be video component */}
 
       <ProjectImageCard
-        image={dgapppic}
-        projectTitle={'Dangerous Goods Project'}
+        image={quirkymentors}
+        projectTitle={'Quirky Mentors Project - Create Camp Auckland 2023'}
         //escape characters won't work in JSX. So split the lines into multiple props or implement another solution.
         // see here: https://stackoverflow.com/questions/67616946/line-break-react-prop
         // and here: https://stackoverflow.com/questions/54197364/react-text-prop-linebreak?rq=3
         // bullet point: &bull;
         projectDescription={
           <text>
+            Live Demo:{' '}
+            <a href="https://quirky-mentors.vercel.app/" target="_blank">
+              Click here
+            </a>
+            <br />
+            <br />
+            I attended Summer of Tech's 2-day event called "Create Camp" where
+            students get paired up with other awesome students to make an
+            interesting project based on using an API.
+            <br />
+            <br />
+            The app idea was to find a way to connect mentors with mentee for
+            either specialized career guidance, or some sort of help for their
+            project to help grow a mentee's skillset. However, as a team it was
+            difficult to justify the reason to make this app. So to solve this,
+            we interviewed a few mentors to ask what would motivate them to use
+            an app like ours.
+            <br />
+            <br />
+            After a few interviews with some mentors, we found that the most
+            valuable reason to use this app was to seamlessly scout for new
+            talent while also providing unfiltered career guidance.
+            <br />
+            <br />
+            For our project, we decided to create an app where the goal was to
+            enable students to effectively seek out mentors to either get career
+            guidance or some help on their project. Currently, most students can
+            only get help from their university's career counselling department,
+            or through Summer of Tech's "1-on-1 Super Mentor" program during
+            2022. So there existed a special need to connect a mentee with a
+            mentor.
+            <br />
+            <br />
+            Now, let's highlight some of the work I did on this project:
+            <br />
+            <br />
+            &bull; Interviewed mentors and mentees to gather motivation to using
+            the app.
+            <br />
+            &bull; Implemented Axios to make requests to iScraper API and gather
+            LinkedIn profile data.
+            <br />
+            &bull; Filtered LinkedIn profile data to create an anoynymized
+            dataset so that the frontend team can create the app UI.
+            <br />
+            &bull; Deployed the live app using Vercel
+            <br />
+            &bull; Assisted teammates with Git to solve merge conflicts and
+            improve productivity
+          </text>
+        }
+      />
+
+      <ProjectImageCard
+        image={dgapppic}
+        projectTitle={'Dangerous Goods Project'}
+        // worked on: backend, API requests, created a false data set based on real linkedin profile data, deployed the app,
+        projectDescription={
+          <text>
+            Note: Due to a Non-Disclosure Agreement, I'm not permitted to share
+            any pictures of the app hence why you're seeing a "NDA" image.
+            <br />
+            <br />
             During my final year of my BCIS degree at AUT, I undertook the
             Dangerous Goods Project, which was part of my Research and
             Development programme that all BCIS students must undertake before
@@ -59,10 +124,14 @@ function App() {
             <br />
             The project involved enhancing a prototype made by the previous R&D
             team of this project, as we were tasked with enhancing their
-            segregation algorithm.
+            segregation algorithm. However, due to a poorly organized codebase
+            our team simply re-wrote the app from scratch. We added new
+            features:
             <br />
-            <br />
-            &bull; Bullet point 1
+            &bull; Improved UI: Our team reworked the previous team's UI to make
+            it easier to understand what the user must do at each screen. &bull;
+            Improved UI: Our team reworked the previous team's UI to make it
+            easier to understand what the user must do at each screen.
           </text>
         }
       />
